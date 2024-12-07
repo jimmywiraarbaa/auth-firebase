c2e47a2f489e769f7e65e4a536dcf573271e8940
Jimmy Wira Arbaa <jimmywiraarbaa03@gmail.com>
2024-12-08 03:06:32 +0700

    cleaning

--- src/config/firebase.js
+++
@@ -1,9 +0,0 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('../../serviceAccountKey.json');
-
-// Inisialisasi Firebase Admin
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-module.exports = admin;
